62c163c5042a26c46579d13f67663b2f8bee26c1
animsh <animsh.more@gmail.com>
2021-01-10 23:00:56 +0530

    changelog: 1. jetpack navigation component setup

--- app/src/main/java/com/animsh/newsdeap/MainActivity.java
+++ app/src/main/java/com/animsh/newsdeap/MainActivity.java
@@ -3,12 +3,25 @@ package com.animsh.newsdeap;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavHostFragment navHostFragment;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Navigation Component
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
     }
 }
---
+++ app/src/main/java/com/animsh/newsdeap/NewsFavFragment.java
@@ -0,0 +1,23 @@
+package com.animsh.newsdeap;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class NewsFavFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_fav, container, false);
+    }
+}
