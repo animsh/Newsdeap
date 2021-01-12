afe46771696a3c945d89d7b9d474a25f2954fe2b
animsh <animsh.more@gmail.com>
2021-01-12 16:27:35 +0530

    changelog: 1.News Items layouts design & databinding 2.Item DataBinding & Custom Adapters

---
+++ app/src/main/java/com/animsh/newsdeap/util/BindingAdapters.java
@@ -0,0 +1,48 @@
+package com.animsh.newsdeap.util;
+
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.animsh.newsdeap.R;
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by animsh on 1/12/2021.
+ */
+public class BindingAdapters {
+
+    @BindingAdapter("glide_url")
+    public static void loadImage(ImageView imageView, String url) {
+        Glide.with(imageView.getContext()).load(url).into(imageView);
+    }
+
+    @BindingAdapter("glide_circular_url")
+    public static void loadUserImage(ImageView imageView, String url) {
+        Glide.with(imageView.getContext()).load(url).circleCrop().into(imageView);
+    }
+
+    @BindingAdapter("set_background")
+    public static void setBackground(ImageView imageView, String color) {
+        switch (color) {
+            case "RED":
+                imageView.setBackgroundColor(imageView.getContext().getResources().getColor(R.color.red));
+                break;
+            case "BLACK":
+                imageView.setBackgroundColor(imageView.getContext().getResources().getColor(R.color.material_black));
+                break;
+            case "YELLOW":
+                imageView.setBackgroundColor(imageView.getContext().getResources().getColor(R.color.yellow));
+                break;
+            case "BLUE":
+                imageView.setBackgroundColor(imageView.getContext().getResources().getColor(R.color.blue));
+                break;
+            case "PURPLE":
+                imageView.setBackgroundColor(imageView.getContext().getResources().getColor(R.color.purple));
+                break;
+            default:
+                imageView.setBackgroundColor(imageView.getContext().getResources().getColor(R.color.red));
+
+        }
+    }
+}
