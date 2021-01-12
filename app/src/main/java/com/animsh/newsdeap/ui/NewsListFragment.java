ab2cab23abc890a213147eae2e7553e3da12fec0
animsh <animsh.more@gmail.com>
2021-01-12 16:45:32 +0530

    changelog: 1.Item DataBinding & Custom Adapters

--- app/src/main/java/com/animsh/newsdeap/ui/NewsListFragment.java
+++ app/src/main/java/com/animsh/newsdeap/ui/NewsListFragment.java
@@ -5,16 +5,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.animsh.newsdeap.R;
+import com.animsh.newsdeap.data.FakeDataSource;
+import com.animsh.newsdeap.ui.news.DiffUtilNewsItemCallback;
+import com.animsh.newsdeap.ui.news.NewsListAdapter;
 
 public class NewsListFragment extends Fragment {
+
+    RecyclerView newsRecyclerView;
+    NewsListAdapter adapter;
+
+    public NewsListFragment() {
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        newsRecyclerView = view.findViewById(R.id.rv_news_list);
+        newsRecyclerView.setHasFixedSize(true);
+        newsRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        adapter = new NewsListAdapter(new DiffUtilNewsItemCallback());
+        newsRecyclerView.setAdapter(adapter);
+
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        adapter.submitList(fakeDataSource.getFakeListNews());
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
