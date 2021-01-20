699afbb308864e9ae6f925d74bacce37fa631579
animsh <animsh.more@gmail.com>
2021-01-20 19:08:20 +0530

    changelog: 1.bug fixes

--- app/src/main/java/com/animsh/newsdeap/ui/NewsDetailsFragment.java
+++ app/src/main/java/com/animsh/newsdeap/ui/NewsDetailsFragment.java
@@ -1,9 +1,11 @@
 package com.animsh.newsdeap.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.animsh.newsdeap.R;
 import com.animsh.newsdeap.data.Article;
@@ -11,11 +13,28 @@ import com.animsh.newsdeap.databinding.FragmentNewsDetailsBinding;
 
 public class NewsDetailsFragment extends AppCompatActivity {
 
+    SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Article article = (Article) getIntent().getSerializableExtra("Article");
         FragmentNewsDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.fragment_news_details);
         binding.setNewsItemData(article);
+
+        swipeRefreshLayout = findViewById(R.id.newsLisSwipe);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        binding.setNewsItemData(article);
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 500);
+            }
+        });
     }
 }
--- app/src/main/java/com/animsh/newsdeap/ui/news/NewsListAdapter.java
+++ app/src/main/java/com/animsh/newsdeap/ui/news/NewsListAdapter.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -74,7 +73,6 @@ public class NewsListAdapter extends ListAdapter<Article, BaseViewHolder> {
                 Intent intent = new Intent(holder.itemView.getContext(), NewsDetailsFragment.class);
                 intent.putExtra("Article", getItem(position));
                 holder.itemView.getContext().startActivity(intent);
-                Toast.makeText(holder.itemView.getContext(), getItem(position).getTitle(), Toast.LENGTH_LONG).show();
             }
         });
     }
